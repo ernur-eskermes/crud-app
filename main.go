1723e161ccee44d11608917297414e15f19f874d
Ernur Eskermes <eskermesernur86@gmail.com>
2022-04-04 22:00:20 +0600

    fixes

--- main.go
+++ main.go
@@ -23,6 +23,22 @@ const schema string = `
 	);
 `
 
+type response struct {
+	Message string `json:"message"`
+}
+
+func newResponse(w http.ResponseWriter, statusCode int, message string) {
+	w.WriteHeader(statusCode)
+	if message != "" {
+		res, err := json.Marshal(response{message})
+		if err != nil {
+			log.Fatalf("Error happened in JSON marshal. Err: %s", err.Error())
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(res)
+	}
+}
+
 func main() {
 	db, err := sqlx.Connect("postgres", "host=ninja-db user=postgres dbname=postgres password=qwerty123 sslmode=disable")
 	if err != nil {
@@ -37,12 +53,14 @@ func main() {
 		case http.MethodGet:
 			books, err := repository.GetBooks(*db)
 			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
+				log.Println(err.Error())
+				newResponse(w, http.StatusInternalServerError, "")
 				return
 			}
 			b, err := json.Marshal(books)
 			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
+				log.Println(err.Error())
+				newResponse(w, http.StatusInternalServerError, "")
 				return
 			}
 			w.Write(b)
@@ -50,23 +68,18 @@ func main() {
 			var book domain.Book
 			reqBytes, err := ioutil.ReadAll(r.Body)
 			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
+				log.Println(err.Error())
+				newResponse(w, http.StatusInternalServerError, "")
 				return
 			}
-
 			if err = json.Unmarshal(reqBytes, &book); err != nil {
-				res, err := json.Marshal(map[string]string{"detail": err.Error()})
-				if err != nil {
-					log.Fatalf("Error happened in JSON marshal. Err: %s", err.Error())
-				}
-				w.WriteHeader(http.StatusBadRequest)
-				w.Header().Set("Content-Type", "application/json")
-				w.Write(res)
+				newResponse(w, http.StatusBadRequest, err.Error())
 				return
 			}
 
 			if err := repository.CreateBook(*db, book); err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
+				log.Println(err.Error())
+				newResponse(w, http.StatusInternalServerError, "")
 				return
 			}
 			w.WriteHeader(http.StatusCreated)
@@ -76,67 +89,69 @@ func main() {
 		params := r.URL.Query()
 		id, err := strconv.Atoi(params.Get("id"))
 		if err != nil {
-			log.Fatalf("Unable to convert the string into int.  %v", err)
+			newResponse(w, http.StatusBadRequest, "Unable to convert the string into int.")
+			return
 		}
 		switch r.Method {
 		case http.MethodGet:
 			book, err := repository.GetBookById(*db, id)
 			if err != nil {
 				if errors.Is(err, repository.ErrBookNotFound) {
-					w.WriteHeader(http.StatusNotFound)
-				} else {
-					w.WriteHeader(http.StatusInternalServerError)
+					newResponse(w, http.StatusNotFound, "")
+					return
 				}
+				log.Println(err.Error())
+				newResponse(w, http.StatusInternalServerError, "")
 				return
 			}
 			b, err := json.Marshal(book)
 			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
+				log.Println(err.Error())
+				newResponse(w, http.StatusInternalServerError, "")
 				return
 			}
 			w.Write(b)
 		case http.MethodPut:
 			if _, err := repository.GetBookById(*db, id); err != nil {
 				if errors.Is(err, repository.ErrBookNotFound) {
-					w.WriteHeader(http.StatusNotFound)
-				} else {
-					w.WriteHeader(http.StatusInternalServerError)
+					newResponse(w, http.StatusNotFound, "")
+					return
 				}
+				log.Println(err.Error())
+				newResponse(w, http.StatusInternalServerError, "")
 				return
 			}
 			var book domain.Book
 			reqBytes, err := ioutil.ReadAll(r.Body)
 			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
+				log.Println(err.Error())
+				newResponse(w, http.StatusInternalServerError, "")
 				return
 			}
 
 			if err = json.Unmarshal(reqBytes, &book); err != nil {
-				res, err := json.Marshal(map[string]string{"detail": err.Error()})
-				if err != nil {
-					log.Fatalf("Error happened in JSON marshal. Err: %s", err.Error())
-				}
-				w.WriteHeader(http.StatusBadRequest)
-				w.Header().Set("Content-Type", "application/json")
-				w.Write(res)
+				newResponse(w, http.StatusBadRequest, err.Error())
+				return
 			}
 			book.Id = id
 			if err = repository.UpdateBook(*db, book); err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
+				log.Println(err.Error())
+				newResponse(w, http.StatusInternalServerError, "")
 				return
 			}
 			return
 		case http.MethodDelete:
 			if _, err = repository.GetBookById(*db, id); err != nil {
 				if errors.Is(err, repository.ErrBookNotFound) {
-					w.WriteHeader(http.StatusNotFound)
-				} else {
-					w.WriteHeader(http.StatusInternalServerError)
+					newResponse(w, http.StatusNotFound, "")
 				}
+				log.Println(err.Error())
+				newResponse(w, http.StatusInternalServerError, "")
 				return
 			}
 			if err = repository.DeleteBook(*db, id); err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
+				log.Println(err.Error())
+				newResponse(w, http.StatusInternalServerError, "")
 				return
 			}
 			w.WriteHeader(http.StatusNoContent)
@@ -144,6 +159,7 @@ func main() {
 	})
 
 	if err = http.ListenAndServe(":8000", nil); err != nil {
+		log.Println(err.Error())
 		log.Fatal(err)
 	}
 }
